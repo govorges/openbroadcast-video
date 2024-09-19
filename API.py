e987641db8e7032c6fb823d69f0407fbbb519cbc
gianaut <gio@gov.org.es>
2024-09-19 04:52:09 -0400

    /videos/thumbnail-upload
    Separate thumbnail upload route (useful for Stream library uploads)

--- API.py
+++ API.py
@@ -51,6 +51,24 @@ def uploads__Capture():
         return make_response("Upload not captured. Video not registered.", 400)
     return make_response("Upload captured. Video registered.", 200)
 
+@api.route("/videos/thumbnail-upload", methods=["POST"])
+def videos__ThumbnailUpload():
+    target_file_path = request.headers.get("target-file-path")
+    if target_file_path is None or target_file_path == "":
+        response_text = BuildJSONResponseText("WARNING", "The header \"target-file-path\" is not set or was set incorrectly", route="/videos/thumbnail-upload", method="POST")
+        return make_response(response_text, 400)
+
+    local_file_path = request.headers.get("local-file-path")
+    if local_file_path is None or local_file_path == "":
+        response_text = BuildJSONResponseText("WARNING", "The header \"local-file-path\" is not set or was set incorrectly", route="/videos/thumbnail-upload", method="POST")
+        return make_response(response_text, 400)
+    
+    status_code = api_VideoHandle.bunny.file_QueueUpload(target_file_path=target_file_path, local_file_path=local_file_path)
+
+    if status_code == 200:
+        return make_response("Success", 200)
+    return make_response("Fail", status_code)
+
 @api.route("/videos/ingest", methods=["POST"])
 def videos__Ingest():
     id = request.headers.get("id")
